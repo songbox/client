090257ff16749f527e0678af20a6d53908d65081
Andreas Böhrnsen <andreas@frontrunner.io>
2016-10-03 00:44:26 +0200

    enable mirage in development

--- config/environment.js
+++ config/environment.js
@@ -20,12 +20,17 @@ module.exports = function(environment) {
     }
   };
 
+  ENV['ember-cli-mirage'] = {
+  };
+
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+
+    ENV['ember-cli-mirage'].enabled = true;
   }
 
   if (environment === 'test') {
