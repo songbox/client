5d1cd745dc4bc1272583e56032fea6c87a040321
Andreas Böhrnsen <andreas@frontrunner.io>
2016-10-29 20:38:33 +0200

    fix sass import

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,7 +7,7 @@ module.exports = function(defaults) {
     // Add options here
     sassOptions: {
       includePaths: [
-        'bower_components/materialize/sass'
+        'bower_components/materialize/sass/components'
       ]
     }
   });
