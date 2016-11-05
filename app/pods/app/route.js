f3bfe2fa4c0db6373d4e4b0cb8dfdb6bac88d61c
Andreas Böhrnsen <andreas@frontrunner.io>
2016-11-05 13:57:01 +0100

    join room as user with userId

--- app/pods/app/route.js
+++ app/pods/app/route.js
@@ -6,13 +6,25 @@ const {
 } = Ember;
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  ajax: service(),
   pubsub: service(),
   session: service(),
 
+  model() {
+    return this.get('ajax').request('/user/current').then(data => {
+      this.store.pushPayload(data);
+    });
+  },
+
   setupController(controller, model) {
+    const userId = this.store.peekAll('user').get('firstObject.id')
+
     this.get('pubsub').connectUser();
-    const channel = this.get('pubsub').joinChannel("room:123", { type: 'User' });
-    controller.setProperties({ model, channel });
+    this.get('pubsub').joinChannel(`room:${userId}`, { type: 'User' }).then((channel) => {
+      controller.setProperties({ model, channel });
+    }).catch(err => {
+      console.log(err);
+    });
   },
 
   actions: {
