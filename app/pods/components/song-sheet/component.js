baa7ebf7f7276674ed2f0ccb9091b4c785396f10
Andreas Böhrnsen <andreas@frontrunner.io>
2016-11-05 09:06:48 +0100

    do not show chords to viewer

--- app/pods/components/song-sheet/component.js
+++ app/pods/components/song-sheet/component.js
@@ -6,7 +6,11 @@ const {
 } = Ember;
 
 export default Ember.Component.extend({
+  classNameBindings: ['showChords'],
+
   song: null,
+
+  showChords: true,
 
   sections: computed('song.text', 'song.format', function () {
     const parser = parsers[this.get('song.format') || 'opensong'];
