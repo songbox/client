5a05efcba607e2f88a2d0dbad4dda33fd7debbca
Andreas Böhrnsen <andreas@frontrunner.io>
2016-11-06 09:56:44 +0100

    do not show sectionHeaders in view mode

--- app/pods/components/song-sheet/component.js
+++ app/pods/components/song-sheet/component.js
@@ -6,11 +6,10 @@ const {
 } = Ember;
 
 export default Ember.Component.extend({
-  classNameBindings: ['showChords'],
-
   song: null,
 
   showChords: true,
+  showSectionHeaders: true,
 
   sections: computed('song.text', 'song.format', function () {
     const parser = parsers[this.get('song.format') || 'opensong'];
