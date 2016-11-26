7fc31408909baa41f41c3c8b25ce1408a9e30448
Andreas Böhrnsen <andreas@frontrunner.io>
2016-11-26 16:19:06 +0100

    add search

--- app/pods/components/songs-sidebar/component.js
+++ app/pods/components/songs-sidebar/component.js
@@ -1,5 +1,19 @@
 import Ember from 'ember';
 
+const {
+  computed,
+  isEmpty
+} = Ember;
+
 export default Ember.Component.extend({
-  songs: []
+  songs: [],
+
+  searchTerm: '',
+  filteredSongs: computed('songs.[]', 'searchTerm', function () {
+    return this.get('songs').filter((song) => {
+      const term = this.get('searchTerm');
+      const songTitle = song.get('title');
+      return isEmpty(term) || (new RegExp(term, 'i')).test(songTitle);
+    });
+  })
 });
