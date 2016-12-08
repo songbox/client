f8f6f75409b04db95e6448b2b166ca3b569570d9
Andreas Böhrnsen <andreas@frontrunner.io>
2016-12-08 20:34:14 +0100

    add inobounce

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -36,5 +36,8 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  // no rubber band scrolling on mobile
+  app.import('bower_components/inobounce/inobounce.js');
+
   return app.toTree();
 };
