351c4f1cacd39634cf9effaf509b2e5a1e71c0d4
Andreas Böhrnsen <andreas@frontrunner.io>
2016-12-09 14:26:16 +0100

    remove semicolon

--- app/session-stores/application.js
+++ app/session-stores/application.js
@@ -2,5 +2,5 @@ import CookieStore from 'ember-simple-auth/session-stores/cookie';
 
 export default CookieStore.extend({
   cookieName: 'songbox-session',
-  cookieExpirationTime: 60 * 60 * 24 * 365; // expire cookie after 1 year
+  cookieExpirationTime: 60 * 60 * 24 * 365 // expire cookie after 1 year
 });
