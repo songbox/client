8e0e81815adce6d34105b4fbc7b63a1a26d956fe
Andreas Böhrnsen <andreas@frontrunner.io>
2016-12-09 14:22:52 +0100

    use standard cookie name

--- app/session-stores/application.js
+++ app/session-stores/application.js
@@ -1,6 +1,6 @@
 import CookieStore from 'ember-simple-auth/session-stores/cookie';
 
 export default CookieStore.extend({
-  cookieName: 'songbox:session',
+  cookieName: 'songbox-session',
   cookieExpirationTime: 60 * 60 * 24 * 365; // expire cookie after 1 year
 });
