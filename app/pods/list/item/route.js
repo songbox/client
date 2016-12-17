19f899d8ee4d706a2e012ad6a90a362ea49ee8e0
Andreas Böhrnsen <andreas@frontrunner.io>
2016-12-17 22:08:40 +0100

    use rsvp.hash to add list model

--- app/pods/list/item/route.js
+++ app/pods/list/item/route.js
@@ -1,21 +1,27 @@
 import Ember from 'ember';
 
 const {
-  inject: { service }
+  inject: { service },
+  RSVP
 } = Ember;
 
 export default Ember.Route.extend({
   flashMessages: service(),
 
   model({ position }) {
-    const listItems = this.modelFor('list').get('listItems');
-    return listItems.sortBy('rank').objectAt(position - 1);
+    const list = this.modelFor('list');
+    const item = list.get('listItems').sortBy('rank').objectAt(position - 1);
+
+    return RSVP.hash({
+      list,
+      item
+    });
   },
 
   afterModel(model) {
-    if (! model) {
+    if (! model.item) {
       this.get('flashMessages').danger('Item not found');
-      this.transitionTo('list');
+      this.transitionTo('list', model.list);
     }
   }
 });
