0762603c347a6065d544aa033441a058a102db70
Andreas Böhrnsen <andreas@frontrunner.io>
2017-01-24 22:27:28 +0100

    upgrade to ember 2.11

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,7 +6,7 @@ module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     autoprefixer: {
     },
-    babel: {
+    'ember-cli-babel': {
       includePolyfill: true
     },
     sassOptions: {
