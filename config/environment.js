9bab0c217d21bc05c0aed2e24152111127eff3b2
Andreas Böhrnsen <andreas@frontrunner.io>
2017-02-03 08:01:31 +0100

    add heroku to hostWhitelist

--- config/environment.js
+++ config/environment.js
@@ -33,7 +33,7 @@ module.exports = function(environment) {
   };
 
   ENV['fastboot'] = {
-    hostWhitelist: ['songbox.co', /^localhost:\d+$/]
+    hostWhitelist: ['songbox.co', 'songbox-app.herokuapp.com/', /^localhost:\d+$/]
   };
 
   ENV['ember-simple-auth'] = {
