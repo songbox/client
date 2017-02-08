ce25e60a8029c0c07a378736ede404e1febe538e
Andreas Böhrnsen <andreas@frontrunner.io>
2017-02-08 22:47:38 +0100

    fix whitelist for sonbox domain

--- config/environment.js
+++ config/environment.js
@@ -33,7 +33,7 @@ module.exports = function(environment) {
   };
 
   ENV['fastboot'] = {
-    hostWhitelist: ['songbox.co', 'songbox-app.herokuapp.com', /^localhost:\d+$/]
+    hostWhitelist: ['app.songbox.co', 'songbox-app.herokuapp.com', /^localhost:\d+$/]
   };
 
   ENV['ember-simple-auth'] = {
