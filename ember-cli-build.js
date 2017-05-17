dfc0212aebca18c6ae7f2f9438def0672d35ef65
Andreas Böhrnsen <andreas@frontrunner.io>
2017-05-17 17:48:52 +0200

    remove deprecation warning

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,7 @@ module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     autoprefixer: {
     },
-    babel: {
+    'ember-cli-babel': {
       includePolyfill: true
     },
     fingerprint: {
