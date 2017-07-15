b2876d0a061b9048d674dee675901406dc86ddc5
Andreas Böhrnsen <andreas@frontrunner.io>
2017-07-15 09:26:16 +0200

    store settings in local storage

--- app/services/settings.js
+++ app/services/settings.js
@@ -1,7 +1,40 @@
 import Ember from 'ember';
+import LocalSettingsInterface from 'songbox/local-settings-interface';
+
+const {
+  computed
+} = Ember;
+
+/**
+  Creates a setting being stored
+  @private
+  @method setting
+*/
+function setting(name) {
+  return computed({
+    get(/*key*/) {
+      return this.get(`lsi.settings.${name}`);
+    },
+    set(_key, value) {
+      this.set(`lsi.settings.${name}`, value);
+      return value;
+    }
+  })
+}
 
 export default Ember.Service.extend({
 
-  nightMode: false
+  nightMode: setting('nightMode'),
+  showChords: setting('showChords'),
+  showHeader: setting('showHeader'),
 
+  init() {
+    this._super(...arguments);
+    let lsi = LocalSettingsInterface.create({
+      serializer: 'json',
+      adapter: Ember.testing ? 'local-memory' : 'local-storage',
+      prefix: 'songbox/'
+    });
+    this.set('lsi', lsi);
+  }
 });
