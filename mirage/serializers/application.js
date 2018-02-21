3c4512d5266cc6866a3e15668fa8fb56ca5d7aaa
Andreas Böhrnsen <andreas@frontrunner.io>
2018-02-21 19:16:21 +0100

    fix mirage upgrade

--- mirage/serializers/application.js
+++ mirage/serializers/application.js
@@ -1,4 +1,7 @@
 import { JSONAPISerializer } from 'ember-cli-mirage';
 
 export default JSONAPISerializer.extend({
+  // TODO: check with
+  // http://www.ember-cli-mirage.com/blog/2018/02/01/changing-mirages-default-linkage-data-behavior/
+  alwaysIncludeLinkageData: true
 });
