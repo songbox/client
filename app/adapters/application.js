68b143218cafba4f0723dd149a68d8c7a1040863
Andreas Böhrnsen <andreas@frontrunner.io>
2019-02-18 09:35:13 +0000

    remove authorizer

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,9 +1,16 @@
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+import { isPresent } from '@ember/utils';
 import ENV from '../config/environment';
 
 export default JSONAPIAdapter.extend(DataAdapterMixin, {
   host: ENV.api.host,
   namespace: ENV.api.namespace,
-  authorizer: 'authorizer:oauth2'
+
+  authorize(xhr) {
+    let { access_token } = this.get('session.data.authenticated');
+    if (isPresent(access_token)) {
+      xhr.setRequestHeader('Authorization', `Bearer ${access_token}`);
+    }
+  }
 });
--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -2,8 +2,6 @@ self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
     { handler: "silence", matchId: "ember-polyfills.deprecate-merge" },
-    { handler: "silence", matchId: "ember-simple-auth.session.authorize" },
-    { handler: "silence", matchId: "ember-simple-auth.baseAuthorizer" },
     { handler: "silence", matchId: "ember-runtime.deprecate-copy-copyable" },
     { handler: "silence", matchId: "remove-handler-infos" },
     { handler: "silence", matchId: "array.new-array-wrapper" }
