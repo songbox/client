5374706f1e58662da6eb212150c25890856d5581
Andreas Böhrnsen <andreas@frontrunner.io>
2019-02-18 10:45:18 +0000

    remove deprecation config for now

--- config/deprecation-workflow.js
+++
@@ -1,9 +0,0 @@
-self.deprecationWorkflow = self.deprecationWorkflow || {};
-self.deprecationWorkflow.config = {
-  workflow: [
-    { handler: "silence", matchId: "ember-polyfills.deprecate-merge" },
-    { handler: "silence", matchId: "ember-runtime.deprecate-copy-copyable" },
-    { handler: "silence", matchId: "remove-handler-infos" },
-    { handler: "silence", matchId: "array.new-array-wrapper" }
-  ]
-};
